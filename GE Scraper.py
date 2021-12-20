9094b492f7a3dcb7b6af998c42271d88325c57cd
Alex Papalazaros <acp5135@gmail.com>
2021-12-20 18:54:55 -0500

    Fixed HTML parser, adjusted dataframe to work for pages by categories rather than summary page. Added today's date to the dataframe for time-series use

--- GE Scraper.py
+++ GE Scraper.py
@@ -1,11 +1,12 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+from datetime import date
 
 url = "https://secure.runescape.com/m=itemdb_rs/catalogue?cat=1"
 r = requests.get(url)
-soup = BeautifulSoup(r.content, 'html5lib')
-df = pd.DataFrame(columns=['Item', 'Min', 'Max', 'Median', 'Total'])
+soup = BeautifulSoup(r.content, 'html.parser')
+df = pd.DataFrame(columns=['Item', 'Price', 'Change', 'Date'])
 
 
 def get_string_from_column(columns, columnIndex):
@@ -15,7 +16,7 @@ def get_string_from_column(columns, columnIndex):
 for row in soup.select("div table tbody tr"):
     columns = row.findAll("td")
 
-    df = df.append({'Item': get_string_from_column(columns, 0), 'Min': get_string_from_column(columns, 2), 'Max': get_string_from_column(
-    columns, 3), 'Median': get_string_from_column(columns, 4), 'Total': get_string_from_column(columns, 5)}, ignore_index=True)
+    df = df.append({'Item': get_string_from_column(columns, 0), 'Price': get_string_from_column(columns, 2), 'Change': get_string_from_column(
+    columns, 3), 'Date': date.today()}, ignore_index=True)
 
 print(df)
