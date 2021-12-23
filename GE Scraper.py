27d098b60ce5ee233d0e3c68149dfc03013c3cea
Alex Papalazaros <acp5135@gmail.com>
2021-12-23 12:12:53 -0500

    Loads the daily price data into a MongoDB(NoSQL). Added environmental variable for database credentials

--- GE Scraper.py
+++ GE Scraper.py
@@ -2,6 +2,12 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 from datetime import date
+from pymongo import MongoClient
+import pymongo
+import dns
+import os
+
+mongo_pwd = os.environ['MDB_pwd']
 
 url = "https://secure.runescape.com/m=itemdb_rs/catalogue?cat=1"
 r = requests.get(url)
@@ -17,6 +23,14 @@ for row in soup.select("div table tbody tr"):
     columns = row.findAll("td")
 
     df = df.append({'Item': get_string_from_column(columns, 0), 'Price': get_string_from_column(columns, 2), 'Change': get_string_from_column(
-    columns, 3), 'Date': date.today()}, ignore_index=True)
+    columns, 3), 'Date': str(date.today())}, ignore_index=True)
+
+cluster = MongoClient(f"mongodb+srv://acp5135:{mongo_pwd}@cluster0.2sduf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = cluster["RS_Data"]
+collection = db["Daily_items"]
+
+df.reset_index(inplace=True)
 
 print(df)
+post = df.to_dict("records")
+collection.insert_many(post)
